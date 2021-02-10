99472f16f237071830d471987067db5a7d109248
ezgi kütük <ezgikutuk3@gmail.com>
2021-02-10 22:29:11 +0300

    Core içine Result, SuccessERsult ve ErrorResult  eklendi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -16,7 +17,19 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-   
+
+        public IResult Add(Product product)
+        {
+            //business codes
+            if (product.ProductName.Length < 2)
+            {
+                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır");
+            }
+            _productDal.Add(product);
+
+            return new SuccessResult("Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             //İş kodları 
@@ -28,6 +41,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
