e99db41afecffbda0a9ab622fb746b1e70e61259
ezgi kütük <ezgikutuk3@gmail.com>
2021-02-11 00:08:37 +0300

    Ekrana mesaj yazdırma işlemi yapıldı.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Entities.Concrete;
 
 namespace Business.Constants
 {
@@ -8,5 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
     }
 }
