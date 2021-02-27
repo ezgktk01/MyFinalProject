211b7f7088204ee6e65a30ea50deb08d3269293f
ezgi kütük <ezgikutuk3@gmail.com>
2021-02-27 22:54:35 +0300

    Encryption, Hashing, JWT eklendi.

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken//Erişim anahtarı
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }//bitiş süresi
+    }
+}
